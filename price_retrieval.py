50c716134684c2edb967e1f570d25554d1c43484
Haley Morgan <haley.morgan@bostonfusion.com>
2020-06-27 19:05:21 -0400

    change retrieval method

--- price_retrieval.py
+++ price_retrieval.py
@@ -2,6 +2,9 @@ import datetime
 import warnings
 import MySQLdb as mdb
 import requests
+import yfinance as yf
+from yahoofinancials import YahooFinancials
+from pandas_datareader import data as pdr
 
 
 # Connect to the MySQL instance
@@ -20,31 +23,29 @@ def obtain_list_of_db_tickers():
   data = cur.fetchall()
   return [(d[0], d[1]) for d in data]
 
-def get_daily_historic_data_yahoo(ticker, start_date=(2000,1,1), end_date=datetime.date.today().timetuple()[0:3]):
+
+def get_daily_historic_data_yahoo(ticker, start_date='2020-01-01', end_date='2020-02-01'):
   """
   Obtains data from Yahoo Finance and returns a list of tuples
  
   ticker: Yahoo Finance ticker symbol
-  start_date: Start date in (YYYY, M, D) fromat
-  end_date: End date in (YYYY, M, D) format
+  start_date: Start date in YYYY-MM-DD format or datetime
+  end_date: End date in YYYY-MM-DD format or datetime
   """
-  # Construct the Yahoo URL with the correct integer query parameters for start/end dates
-  # Note that some parameters are zero-based
-  ticker_tup = (ticker, start_date[1]-1, start_date[2], start_date[0], end_date[1]-1, end_date[2], end_date[0])
-  
-  yahoo_url = "http://ichart.finance.yahoo.com/table.csv"
-  yahoo_url += "?s=%s&a=%s&b=%s&c=%s&d=%s&e=%s&f=%s"
-  yahoo_url = yahoo_url % ticker_tup
-  
-  # Try connecting to YF and obtaining the data
-  # On failure, print an error message
-  try:
-    yf_data = requests.get(yahoo_url).text.split("\n")[1:-1]
-    prices = []
-    for y in yf_data:
-      p = y.strip().split(',')
-  except Exception as e:
-    print("Could not download Yahoo data: %s" % e)
+
+  # Use yfinance to access all stock data and get it ready to store
+  start = datetime.datetime(2019, 1, 1)
+  end = datetime.datetime.today()
+  yf.pdr_override()
+  data = pdr.get_data_yahoo([ticker], start=start, end=end)
+  prices = []
+  for index, day in data.iterrows():
+    day_info = (index, day['Open'], day['High'], day['Low'], day['Close'], day['Volume'], day['Adj Close'])
+    print(day_info)
+    prices.append(day_info)
+  return prices
+
+
 
 
 if __name__ == "__main__":
@@ -55,5 +56,8 @@ if __name__ == "__main__":
   tickers = obtain_list_of_db_tickers()
   lentickers = len(tickers)
   for i, t in enumerate(tickers):
-    print("Adding data for %s: %s out of %s" % (t[1], i+1, lentickers))
-    yf_data = get_daily_historic_data_yahoo(t[1])
+    if i < 23: continue
+    stock_tk = t[1]
+    if '.' in stock_tk: stock_tk = stock_tk.replace('.', '-')
+    print("Adding data for %s: %s out of %s" % (stock_tk, i+1, lentickers))
+    yf_data = get_daily_historic_data_yahoo(stock_tk)
